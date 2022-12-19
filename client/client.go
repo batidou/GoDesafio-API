2f41d5689931a08f7790db8033397980390d2fa6
Douglas Batista <douglas_bsouza@hotmail.com>
2022-12-19 01:03:35 -0300

    Gravando arquivo com o valor da cotacao

--- client/client.go
+++ client/client.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"time"
 )
 
-func main ()  {
-	ctx, cancel := context.WithTimeout(context.Background(), 400 * time.Millisecond)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
@@ -21,6 +22,29 @@ func main ()  {
 	if err != nil {
 		panic(err)
 	}
+
 	defer res.Body.Close()
-	io.Copy(os.Stdout, res.Body)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	Salva(string(body))
+
+}
+
+func Salva(cotacao string) {
+
+	// cria o arquivo de cotacao
+	f, err := os.Create("cotacao.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// escreve no arquivo
+	_, err = f.WriteString(fmt.Sprintf("Dólar: %s", cotacao))
+	if err != nil {
+
+	}
 }
